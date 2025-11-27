35baf3ee73f548420a2be0c110c2370951eb2a92
Umaru Bayoh <bayohumau66@gmail.com>
2025-11-27 17:26:20 +0000

    modified utils.mjs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // 
-import { createElement } from ./utils.mjs;
+import { createElement } from  "./utils.mjs";
 
 const hello = createElement("h1", {textContent: "Hello World"});
-document.getElementById("root").appendChild(hello);
+document.getElementById("root").appendChild(hiello);
