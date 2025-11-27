995ba5f7fdc4474132ce1de0b0f229e69cb2700d
Umaru Bayoh <bayohumau66@gmail.com>
2025-11-27 17:38:26 +0000

    added utils.mjs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // 
-import { createElement } from  "./utils.mjs";
 
-const hello = createElement("h1", {textContent: "Hello World"});
-document.getElementById("root").appendChild(hiello);
+import { createElement} from "./utils.mjs";
+
+const hello = createElement("h1", {textContent: "Hello World!"});
+document.getElementById("root").appendChild(hello);
--- src/utils.mjs
+++ src/utils.mjs
@@ -1,20 +1,18 @@
-// Helper function for creating element and appending it
 export function createElement(type, props = {}, children = []) {
-// create elemnt
-const element = document.createElement(type);
+  const element = document.createElement(type);
 
-// props: {textContent : "Hello World!", id: "header1", "data-productId": 123, ...}
-object.entries(props).forEach(([key, value]) => {
-if(~key.indexOf("-")) {
-element.setAttribute(key, value); // data attributes
-} else {element[key] = value;
-}
-});
+  // props: {textContent: "Hello world!", id: "header1", "data-productId": 123, ...}
+  Object.entries(props).forEach(([key, value]) => {
+    if(~key.indexOf('-')) {
+      element.setAttribute(key, value); // data attributes
+    } else {
+      element[key] = value;
+    }
+  });
 
-// loop the children elements
-children.forEach(child) =>  {
-element.appendChild(child);
-});
+  children.forEach((child) => {
+    element.appendChild(child);
+  });
 
-return element;
+  return element;
 }
