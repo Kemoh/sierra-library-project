451e37425abd3743759e65e9304149b7c3f931bb
Umaru Bayoh <bayohumau66@gmail.com>
2025-12-09 06:00:14 +0000

    Added environment variable to Netlify

--- src/js/api/fetchQuestions.js
+++ src/js/api/fetchQuestions.js
@@ -1,6 +1,5 @@
 // ALOC API
-const apiKey = process.env.PARCEL_API_KEY;
-console.log("API key loaded:", apiKey);
+const apiKey = process.env.API_KEY;
 
 export async function fetchQuestionV2({ subject, year = null, type = null }) {
   const baseURL = "https://questions.aloc.com.ng/api/v2/q";
